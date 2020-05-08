1332126131f72f8c1cbcebd096e8e89509eb1b52
謡了謡頭 <1216124438@qq.com>
2020-05-08 17:19:58 +0800

    Site updated: 2020-05-08 17:19:56

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,13 +4,13 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '期待下次不期而遇';
+         document.title = '命里必有一别陌路';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
          // document.title = '不忘初心幸得相逢' + OriginTitle;
-         document.title = '不忘初心幸得相逢';
+         document.title = '世间若有一见如故';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
